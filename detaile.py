508b1f130e41b9afaa8313e0fbaf19616c3fce30
Safian Malik <safianmalik520@gmail.com>
2026-01-11 13:06:48 +0500

    well come to world
    Hey well come!!!

---
+++ detaile.py
@@ -0,0 +1,12 @@
+print("hey")
+print("how are you")
+nmf=input("your are first name")
+nml=input("your are last name")
+x=int(input("your age"))
+#WELL_COME!:
+age_1=1
+age_2=2
+if age_1 > age_2 or age_1 < age_2 :
+    print("well come to world")
+else:
+    print("Not finde")    
