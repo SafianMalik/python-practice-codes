dd877fee27a7e068464cde25f01db9b436d57170
Safian Malik <safianmalik520@gmail.com>
2026-01-12 21:04:38 +0500

    Argent Sellecton.....
    Teacher Argent Sellection................................................................................................................................................................................!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Teacher_Sellection.py
@@ -0,0 +1,8 @@
+print("Hello EMPLOYE'S")
+# ARGENT TEACHER JOB ENTERY NOT A MATER GRADE!!!...
+Name=input("What is your name?")
+degree=input("Avable BS degree?")
+if degree == "Yes":
+    print("Welcome you are Sellected " + Name)
+else :
+    print("PLZ Leave This " + Name)
