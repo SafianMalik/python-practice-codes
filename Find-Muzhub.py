7cba793b76b03f29bea6d76476664a1d0f060da5
Safian Malik <safianmalik520@gmail.com>
2026-01-13 19:36:33 +0500

    YOUR MUZHUB.....
    your muzhub

---
+++ Find-Muzhub.py
@@ -0,0 +1,9 @@
+print("Hello...:")
+#  YOUR MUZHUB.../!!!
+print("please enter your muzhub")
+A=str(input())
+B=str(input())
+if A==B:
+    print("The Muzhub is:",A==B)
+else:
+    print("The Muzhub is not finde PLZ same A==B :")
